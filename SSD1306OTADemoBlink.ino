ad76846e3b9c33db85de5317aa5d646844ff9222
Pelikano <leon@berkenheuvel.com>
2016-12-25 16:05:12 +0100

    Demo including blink

---
+++ SSD1306OTADemoBlink.ino
@@ -0,0 +1,96 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 by Daniel Eichhorn
+ * Copyright (c) 2016 by Fabrice Weinberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.  
+ *
+ */ 
+
+// WiFi includes
+ #include <ESP8266WiFi.h>
+
+ // OTA Includes. Use IDE 1.6.7 or higher (https://github.com/esp8266/Arduino/blob/master/doc/ota_updates/readme.md) 
+ #include <ESP8266mDNS.h>
+ #include <ArduinoOTA.h>
+
+// Display includes
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+char *ssid = "your_network";    //  your network SSID (name) //For OTA use the same network for ESP and PC
+char *password = "your_key";   // your network password
+
+// Initialize the OLED display using Wire library
+SSD1306  display(0x3c, 4, 5);   //display(I2C address, SDA, SCL)
+
+void setup() {
+
+  // int BUILTIN_LED = 13; 
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+  
+  WiFi.begin ( ssid, password );
+
+  // Wait for connection
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 10 );
+  }
+
+  display.init();
+  display.flipScreenVertically();
+  display.setContrast(255);
+
+  ArduinoOTA.begin();
+  ArduinoOTA.onStart([]() {
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "OTA Update");
+    display.display();
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    display.drawProgressBar(4, 32, 120, 8, progress / (total / 100) );
+    display.display();
+  });
+
+  ArduinoOTA.onEnd([]() {
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Restart");
+    display.display();
+  });
+
+  // Align text vertical/horizontal center
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Ready for OTA:\n" + WiFi.localIP().toString());
+  display.display();
+}
+ 
+void loop() {
+  ArduinoOTA.handle();
+//void loop()
+  digitalWrite(BUILTIN_LED, HIGH);  // turn on LED with voltage HIGH
+  delay(1000);                      // wait one second
+  digitalWrite(BUILTIN_LED, LOW);   // turn off LED with voltage LOW
+  delay(1000);                      // wait one second
+  
+}
